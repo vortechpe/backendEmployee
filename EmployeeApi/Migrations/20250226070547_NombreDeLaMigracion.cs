6943966ee73ac71ae7dd75a3838fea1493609662
vortech <vortechperu@gmail.com>
2025-02-26 02:08:01 -0500

    first commit

---
+++ EmployeeApi/Migrations/20250226070547_NombreDeLaMigracion.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class NombreDeLaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Employees",
+                newName: "IsActive");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Employees",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "documentoNumber",
+                table: "Employees",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "documentoNumber",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Employees",
+                newName: "Status");
+        }
+    }
+}
